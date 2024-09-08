e431d3568274590b24e0efc1e5abe7c9648d1991
Giada Unger <giada.unger02@gmail.com>
2024-09-08 17:28:21 +0200

    Added some divs where I will display current gas prices

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ function App() {
         <h2 className="text-4xl pb-2">Save More, Drive Smarter!</h2>
         <p className="text-xl">Join our SMS list and get instant alerts when gas prices drop.</p>
       </div>
-      <div className="mt-4">
+      <div className="mt-4 pb-20">
         <form action="" className="flex flex-row items-center space-x-2">
           <input
             type="number"
@@ -22,8 +22,25 @@ function App() {
           </button>
         </form>
       </div>
+      <div>
+        <h2 className="text-center text-2xl pb-4">Current gas prices</h2>
+        <div className="flex flex-row gap-8">
+            <div className="p-4 bg-blue-300 rounded text-center">
+                <h2 className="text-xl">Gas station</h2>
+                <p className="text-lg">Price</p>
+            </div>
+            <div className="p-4 bg-blue-300 rounded text-center">
+                <h2 className="text-xl">Gas station</h2>
+                <p className="text-lg">Price</p>
+            </div>
+            <div className="p-4 bg-blue-300 rounded text-center">
+                <h2 className="text-xl">Gas station</h2>
+                <p className="text-lg">Price</p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
