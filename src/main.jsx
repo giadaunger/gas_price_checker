785090a2f8985c19f647f8ee4e0e00cb64bf1432
Giada Unger <giada.unger02@gmail.com>
2024-09-18 12:32:55 +0200

    Added white borders to the logo pictures

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ function App() {
         <h2 className="text-center text-2xl pb-4">Current gas prices</h2>
         <div className="flex flex-row gap-8">
             <div className="p-4 bg-[#6b7280] rounded text-center shadow-2xl">
-                <img src="/St1_logo.png" alt="logo" className="w-40 h-40 rounded-2xl shadow-2xl" />
+                <img src="/St1_logo.png" alt="logo" className="w-40 h-40 rounded shadow-2xl border-2 border-white" />
                 <div className="flex flex-row p-2 mt-4 shadow-xl bg-[#7b8392] rounded">
                     <p className="flex justify-center items-center">Bensin</p>
                     <p className="bg-yellow-500 p-2 rounded ml-auto font-orbitron font-semibold">12.26</p>
@@ -28,7 +28,7 @@ function App() {
                 </div>
             </div>
             <div className="p-4 bg-[#6b7280] rounded text-center shadow-2xl">
-                <img src="/Circle_k_logo.png" alt="logo" className="w-40 h-40 rounded-2xl shadow-2xl" />
+                <img src="/Circle_k_logo.png" alt="logo" className="w-40 h-40 rounded shadow-2xl" />
                 <div className="flex flex-row p-2 mt-4 shadow-xl bg-[#7b8392] rounded">
                     <p className="flex justify-center items-center">Bensin</p>
                     <p className="bg-yellow-500 p-2 rounded ml-auto font-orbitron font-semibold">12.26</p>
@@ -43,7 +43,7 @@ function App() {
                 </div>
             </div>
             <div className="p-4 bg-[#6b7280] rounded text-center shadow-2xl">
-                <img src="/Preem_logo.png" alt="logo" className="w-40 h-40 rounded-2xl shadow-2xl" />
+                <img src="/Preem_logo.png" alt="logo" className="w-40 h-40 rounded shadow-2xl border-2 border-white" />
                 <div className="flex flex-row p-2 mt-4 shadow-xl bg-[#7b8392] rounded">
                     <p className="flex justify-center items-center">Bensin</p>
                     <p className="bg-yellow-500 p-2 rounded ml-auto font-orbitron font-semibold">12.26</p>
