b51f51b119acefd09b3f107daa492b587a9dce43
Giada Unger <giada.unger02@gmail.com>
2024-09-18 11:06:09 +0200

    Removed the form and added a regular button instead.

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,15 @@ import './index.css';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className="flex flex-col mx-auto justify-center items-center min-h-screen w-2/3">
       <div className="text-center pb-4">
         <h2 className="text-4xl pb-2">Save More, Drive Smarter!</h2>
         <p className="text-xl">Join our SMS list and get instant alerts when gas prices drop.</p>
+        <button className="p-4 bg-blue-500 text-white rounded mt-8 text-xl">Join SMS list!</button>
       </div>
-      <div className="mt-4 pb-20">
-        <form action="" className="flex flex-row items-center space-x-2">
-          <input
-            type="number"
-            name="phoneNbr"
-            id="phoneNbr"
-            placeholder="Enter your phone number"
-            className="p-2 border border-gray-300 rounded"
-          />
-          <button type="submit" className="p-2 bg-blue-500 text-white rounded">
-            Join SMS list!
-          </button>
-        </form>
-      </div>
-      <div>
+      <div className="mt-12 w-1/2">
         <h2 className="text-center text-2xl pb-4">Current gas prices</h2>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-col gap-8">
             <div className="p-4 bg-blue-300 rounded text-center">
                 <h2 className="text-xl">Gas station</h2>
                 <p className="text-lg">Price</p>
